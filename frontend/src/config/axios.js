d292c56a9063cbcd580234dc38cf015f8a6acc45
danielace1 <danielsudharsan1805@gmail.com>
2026-02-27 04:32:31 +0530

    fix: deploy

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -2,7 +2,10 @@ import axios from "axios";
 import { useAuthStore } from "../store/useAuthStore";
 
 const axiosInstance = axios.create({
-  baseURL: `${import.meta.env.VITE_API_URL || "http://localhost:5001/api"}`,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
   withCredentials: true,
 });
 
